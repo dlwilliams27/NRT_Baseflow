069b77c49dda96d5c2cc34346fe9ecb8d2606550
Delanie Williams <dlwilliams27@crimson.ua.edu>
2025-12-16 11:24:05 -0600

    debug all pre-pro

--- Post_Processing.py
+++ Post_Processing.py
@@ -27,18 +27,17 @@ def nwm_processing(nwm_path, nwm_path_list):
     for folder in nwm_path.iterdir():
         match1=re.match(r'region_(\d+)',folder.name)
         if match1:
-            path= nwm_path / f'{match1[0]}'
-            nwm_path_list.append(path)
+            path1= nwm_path / f'{match1[0]}'
+            nwm_path_list.append(path1)
     #iterating through files in each region folder
     for subfolder in nwm_path_list:
+        print("subfolder", subfolder)
         # issue is that these are glob objects and not iterable, need them to be pure paths
-        for file in subfolder.glob('.nc'):
-            print("The files in the subfolder", file)
+        for file in subfolder.glob('*.nc'):
             match = re.match(r"NWM_gage_(\d+)", file.name)
-            print("The match is:",match)
             if match:
                 gage = match[1]
-                ds=xr.open_dataset(nc)
+                ds=xr.open_dataset(file)
                 nwm = ds.to_dataframe()
                 nwm['baseflow'] = nwm['q_lateral'] + nwm['qBucket']
                 nwm.reset_index(inplace=True)
@@ -51,32 +50,41 @@ def nwm_processing(nwm_path, nwm_path_list):
                 print(f"No match for file {nc}.")
     return nwm_dic
 
+#process through each file in usgs streamflow
 def usgs_processing(usgs_path):
     stream_path= usgs_path / "USGS_Streamflow_2024"
     for folder in stream_path.iterdir():
         match1=re.match(r'(\d+)',folder.name)
         if match1:
-            path2= usgs_path / f'{match1.group(1)}'
+            path2= stream_path / f'{match1.group(1)}'
             usgs_path_list.append(path2)
-
-    for subfolder in nwm_path_list:
-        all_files=subfolder.glob('.txt')
-        for txt in all_files:
-            match2 = re.match(r"(\d+)_streamflow_qc", txt.name)
+    for subfolder in usgs_path_list:
+        for file in subfolder.glob('*.txt'):
+            match2 = re.match(r"(\d+)_streamflow_qc", file.name)
             if match2:
                 gage2=match2.group(1)
-                usgs = pd.read_csv(txt, sep=' ', on_bad_lines='skip')
-                usgs.columns = ['gage', 'year', 'month', 'day', 'NAN', 'nan', 'Q', 'nAn']
+                usgs = pd.read_csv(file, sep=' ', on_bad_lines='skip')
+                if len(usgs.columns) == 7:
+                    usgs.columns = ['gage', 'year', 'month', 'day', 'NAN', 'Q', 'nAn']
+                elif len(usgs.columns) == 8:
+                    usgs.columns = ['gage', 'year', 'month', 'day', 'NAN', 'nan', 'Q', 'nAn']
+                elif len(usgs.columns) == 9:
+                    usgs.columns = ['gage', 'year', 'month', 'day', 'NAN', 'nan', 'NaN', 'Q', 'nAn']
+                elif len(usgs.columns) == 10:
+                    usgs.columns = ['gage', 'year', 'month', 'day', 'NAN', 'nan', 'NaN', 'NAn', 'Q', 'nAn']
                 usgs = usgs[['gage', 'year', 'month', 'day', 'Q']]
+                usgs.dropna(inplace=True)
+                usgs['day']=usgs['day'].astype(int)
                 usgs['date'] = pd.to_datetime(usgs[['year', 'month', 'day']])
                 usgs = usgs[['date', 'Q']]
-                usgs_dic['gage2'] = usgs
+                usgs_dic[gage2] = usgs
     return usgs_dic
 
+#process through each transformed eckhardt baseflow file
 def eck_processing(usgs_path):
     eck_path = usgs_path / "Eckhardt_2024"
-    all_eck_files=eck_path.glob('.csv')
-    for file in all_eck_files:
+    print("eckpath", eck_path)
+    for file in eck_path.glob('*.csv'):
         match3 = re.match(r"(\d+)_streamflow_qc_processed", file.name)
         if match3:
             gage=match3.group(1)
@@ -85,10 +93,9 @@ def eck_processing(usgs_path):
             eck_dic[gage]=eck
     return eck_dic
 
-
 if __name__ == '__main__':
-    nwm_dic=nwm_processing(nwm_path, nwm_path_list)
-    usgs_dic=usgs_processing(usgs_path)
+    #nwm_dic=nwm_processing(nwm_path, nwm_path_list)
+    #usgs_dic=usgs_processing(usgs_path)
     eck_dic=eck_processing(usgs_path)
     print(nwm_dic)
     print(usgs_dic)
