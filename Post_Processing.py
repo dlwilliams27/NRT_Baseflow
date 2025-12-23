078d09f5131800213a08536ec1f98a0b02a374e7
Delanie Williams <dlwilliams27@crimson.ua.edu>
2025-12-23 16:50:10 -0600

    stats edits 2

--- Post_Processing.py
+++ Post_Processing.py
@@ -48,7 +48,7 @@ def nwm_processing(nwm_path, nwm_path_list):
                 nwm_avg_base = nwm.groupby('date', sort=True)['baseflow'].mean()
                 nwm_avg_stream = nwm.groupby('date', sort=True)['streamflow'].mean()
                 nwm_tot = pd.merge(nwm_avg_base, nwm_avg_stream, left_index=True, right_index=True)
-                nwm_tot.reset_index(inplace=True, columns='date')
+                nwm_tot.reset_index(inplace=True)
                 nwm_dic[gage] = nwm_tot
             else:
                 print(f"No match for file {nc}.")
@@ -112,114 +112,88 @@ def merge_dicts(nwm_dic, usgs_dic, eck_dic):
         final=pd.merge(middle, df3, left_index=True, right_index=True)
         complete.append(final)
         print(final)
-    pd.concat(complete)
-    complete['BFIobs']=(complete['Eckhardt'].astype(float))/complete['Q'] #issue with data types here
+    complete=pd.concat(complete, ignore_index=False)
+    print(complete['Eckhardt'].dtypes)
+    print(complete['Q'].dtypes)
+    complete['BFIobs']=(complete['Eckhardt'].astype(float))/(complete['Q'].astype(float)) #issue with data types here
     complete['BFIsim']=complete['baseflow']/complete['streamflow']
 
     finaloutput_path = base_path / 'Complete_inputs4stats.csv'
     finaloutput_path.parent.mkdir(parents=True, exist_ok=True)
     complete.to_csv(finaloutput_path, index=True)
 
-    return final, common_keys
+    return complete, common_keys
 
 def seasons(df):
-    df.iterrows()
-        if df['month']= 12 or 1 or 2:
-            df['season']=1
-        elif df['month']== 3 or 4 or 5:
-            df['season']=2
-        elif df['month']== 6 or 7 or 8:
-            df['season']=3
-        else:
-            df['season']=4
-    #return df not working???
+    conditions = [
+        df['month'].isin([12, 1, 2]),
+        df['month'].isin([3, 4, 5]),
+        df['month'].isin([6, 7, 8]),
+        df['month'].isin([9, 10, 11]),
+    ]
+    choices = [1, 2, 3, 4]
+    df['season'] = np.select(conditions, choices, default=np.nan)
+    return df
 
-def stats(final_df, common_keys):
+def stats(final_df):
     final_df.reset_index(inplace=True)
     final_df['year']=final_df['date'].dt.year
     final_df['month']=final_df['date'].dt.month
     final_df=seasons(final_df)
     final_stats=pd.DataFrame()
-    grouped=final_df.groupby('gage')
-    #overall stats
-    for name, group in grouped:
-        final_stats['gage']=name
-        final_stats['nse_o']=he.nse(group['baseflow'].to_numpy(), group['Eckhardt'].to_numpy())
-        final_stats['kge_12_o'] = he.kge_2012(group['baseflow'].to_numpy(), group['Eckhardt'].to_numpy())
-        final_stats['rmse_o']=he.rmse(group['baseflow'],group['Eckhardt'])
-        final_stats['pearson_o']=he.pearson_r(group['baseflow'],group['Eckhardt'])
+
+    #empty list
+    rows=[]
+    rows2=[]
+    rows3=[]
+
+    # overall stats
+    grouped=final_df.groupby(['gage'])
+    for gage, group in grouped:
+        rows.append({'gage': gage, 'nse_o': he.nse(group['baseflow'].to_numpy(), group['Eckhardt'].to_numpy()),
+                     'rmse_o': he.rmse(group['baseflow'],group['Eckhardt']),
+                     'kge_12_o': he.kge_2012(group['baseflow'].to_numpy(), group['Eckhardt'].to_numpy()),
+                     'pearson_o': he.pearson_r(group['baseflow'],group['Eckhardt'])}
+        )
+    overall_stats=pd.DataFrame(rows)
     overall_path= base_path / 'Overall_stats.csv'
     overall_path.parent.mkdir(parents=True, exist_ok=True)
-    final_stats.to_csv(overall_path, index=True)
+    overall_stats.to_csv(overall_path, index=True)
+
     #year stats
-    grouped=final_df.groupby(['gage', 'year'])
-    # how to iterate with two "names"
-        final_stats['gage'] = name
-        final_stats['nse_y'] = he.nse(group['baseflow'].to_numpy(), group['Eckhardt'].to_numpy())
-        final_stats['kge_12_y'] = he.kge_2012(group['baseflow'].to_numpy(), group['Eckhardt'].to_numpy())
-        final_stats['rmse_y'] = he.rmse(group['baseflow'], group['Eckhardt'])
-        final_stats['pearson_y'] = he.pearson_r(group['baseflow'], group['Eckhardt'])
+    grouped2=final_df.groupby(['gage', 'year'])
+    for (gage, year), group2 in grouped2:
+        rows2.append({'gage': gage, 'year': year, 'nse_o': he.nse(group2['baseflow'].to_numpy(), group2['Eckhardt'].to_numpy()),
+                     'rmse_o': he.rmse(group2['baseflow'], group2['Eckhardt']),
+                     'kge_12_o': he.kge_2012(group2['baseflow'].to_numpy(), group2['Eckhardt'].to_numpy()),
+                     'pearson_o': he.pearson_r(group2['baseflow'], group2['Eckhardt'])}
+                    )
+    year_stats=pd.DataFrame(rows2)
     year_path = base_path / 'year_stats.csv'
     year_path.parent.mkdir(parents=True, exist_ok=True)
-    final_stats.to_csv(year_path, index=True)
-    #seasonal stats #want this to be grouped with w=(12,1,2)=Dec,Jan,Feb s=(3,4,5)=Mar,Apr,May su=(6,7,8)=Jun,Jul,Aug f=(9,10,11)=Sep, Oct, Nov
-    grouped=final_df.groupby(['gage', 'season'])
-    #how to iterate with two "names"
-        final_stats['gage'] = name
-        final_stats['nse_s'] = he.nse(group['baseflow'].to_numpy(), group['Eckhardt'].to_numpy())
-        final_stats['kge_12_s'] = he.kge_2012(group['baseflow'].to_numpy(), group['Eckhardt'].to_numpy())
-        final_stats['rmse_s'] = he.rmse(group['baseflow'], group['Eckhardt'])
-        final_stats['pearson_s'] = he.pearson_r(group['baseflow'], group['Eckhardt'])
+    year_stats.to_csv(year_path, index=True)
+
+    #adds in season column
+    final_df=seasons(final_df)
+    grouped3=final_df.groupby(['gage', 'season'])
+    for (gage, season), group3 in grouped3:
+        rows3.append(
+            {'gage': gage, 'season': season, 'nse_o': he.nse(group3['baseflow'].to_numpy(), group3['Eckhardt'].to_numpy()),
+             'rmse_o': he.rmse(group3['baseflow'], group3['Eckhardt']),
+             'kge_12_o': he.kge_2012(group3['baseflow'].to_numpy(), group3['Eckhardt'].to_numpy()),
+             'pearson_o': he.pearson_r(group3['baseflow'], group3['Eckhardt'])}
+            )
+    season_stats = pd.DataFrame(rows3)
     season_path = base_path / 'seasonal_stats.csv'
     season_path.parent.mkdir(parents=True, exist_ok=True)
-    final_stats.to_csv(season_path, index=True)
-
-#for example purposes and so I don't have to run all the previous functions
-nwm_ex=r"C:\Users\Delanie Williams\OneDrive - The University of Alabama\Research\Baseflow Project\NWM_Results\region_3\NWM_gage_2046000.nc"
-usgs_ex=r"C:\Users\Delanie Williams\OneDrive - The University of Alabama\Research\Baseflow Project\Initial_Results\USGS_Streamflow_2024\03\02046000_streamflow_qc.txt"
-eck_ex=r"C:\Users\Delanie Williams\OneDrive - The University of Alabama\Research\Baseflow Project\Initial_Results\Eckhardt_2024\02046000_streamflow_qc_processed.csv"
-
-ds=xr.open_dataset(nwm_ex)
-nwm = ds.to_dataframe()
-nwm['baseflow'] = nwm['q_lateral'] + nwm['qBucket']
-nwm.reset_index(inplace=True)
-nwm['date'] = nwm['time'].dt.date
-nwm_avg_base = nwm.groupby('date', sort=True)['baseflow'].mean()
-nwm_avg_stream = nwm.groupby('date', sort=True)['streamflow'].mean()
-nwm_tot = pd.merge(nwm_avg_base, nwm_avg_stream, left_index=True, right_index=True)
-nwm_dic_sam= {'02046000':nwm_tot}
-
-usgs = pd.read_csv(usgs_ex, sep=' ', on_bad_lines='skip')
-if len(usgs.columns) == 7:
-    usgs.columns = ['gage', 'year', 'month', 'day', 'NAN', 'Q', 'nAn']
-elif len(usgs.columns) == 8:
-    usgs.columns = ['gage', 'year', 'month', 'day', 'NAN', 'nan', 'Q', 'nAn']
-elif len(usgs.columns) == 9:
-    usgs.columns = ['gage', 'year', 'month', 'day', 'NAN', 'nan', 'NaN', 'Q', 'nAn']
-elif len(usgs.columns) == 10:
-    usgs.columns = ['gage', 'year', 'month', 'day', 'NAN', 'nan', 'NaN', 'NAn', 'Q', 'nAn']
-usgs = usgs[['gage', 'year', 'month', 'day', 'Q']]
-usgs.dropna(inplace=True)
-usgs['day']=usgs['day'].astype(int)
-usgs['date'] = pd.to_datetime(usgs[['year', 'month', 'day']])
-usgs = usgs[['date', 'Q']]
-usgs.set_index('date', inplace=True)
-usgs_dic_sam= {'02046000':usgs}
-
-eck=pd.read_csv(eck_ex)
-eck['date'] = pd.to_datetime(eck['date'])
-eck.set_index('date', inplace=True)
-eck_dic_sam= {'02046000': eck}
-print(nwm_dic_sam['02046000'])
-print(usgs_dic_sam['02046000'])
-print(eck_dic_sam['02046000'])
+    season_stats.to_csv(season_path, index=True)
 
 if __name__ == '__main__':
-    #nwm_dic=nwm_processing(nwm_path, nwm_path_list)
-    #usgs_dic=usgs_processing(usgs_path)
-    #eck_dic=eck_processing(usgs_path)
-    all, common_keys=merge_dicts(nwm_dic=nwm_dic_sam, usgs_dic=usgs_dic_sam, eck_dic=eck_dic_sam)
-    stats(all, common_keys)
+    nwm_dic=nwm_processing(nwm_path, nwm_path_list)
+    usgs_dic=usgs_processing(usgs_path)
+    eck_dic=eck_processing(usgs_path)
+    all, common_keys=merge_dicts(nwm_dic=nwm_dic, usgs_dic=usgs_dic, eck_dic=eck_dic)
+    stats(all)
 
 
 
