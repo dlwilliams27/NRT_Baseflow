8bba1a13c06589ababd6b0076524be88a2175e95
Delanie Williams <dlwilliams27@crimson.ua.edu>
2026-01-15 13:28:42 -0600

    experimenting

--- Actual_SHAP.py
+++ Actual_SHAP.py
@@ -1,4 +1,3 @@
-
 import shap
 import pandas as pd
 from random import randint
@@ -7,10 +6,16 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import RandomizedSearchCV
 
 #can use lists, numpy arrays, or pd df
-path=r""
+path=r"C:\Users\Delanie Williams\OneDrive - The University of Alabama\Research\Baseflow Project\Camels_Stats_Combined.csv"
 data=pd.read_csv(path)
-inputs=data[0:7] #change based off column ids
-outputs=data[7:]
+
+#setting-up input and output datasets
+inputs=data.iloc[:,5:]
+outputs=data.iloc[:,1:5]
+inputs['gage']=data['gage']
+outputs['gage']=data['gage']
+outputs['gage']=outputs['gage'].astype(str).str.zfill(8)
+inputs['gage']=inputs['gage'].astype(str).str.zfill(8)
 
 #initializing the model
 X_train, X_test, y_train, y_test = train_test_split(inputs, outputs, test_size=0.25)
@@ -18,10 +23,10 @@ model = RandomForestRegressor()
 
 #dictionary for the randomsearchcv, link hyper-parameters with ranges to try
 param_grid= {
-    'n_estimators': randint(20,200),
+    'n_estimators': 10,
     "criterion": "squared_error",
-    'min_samples_split': randint(1,10),
-    'min_samples_leaf': randint(1,10)
+    'min_samples_split': 10,
+    'min_samples_leaf': 10
 }
 
 #instance of the class
@@ -31,8 +36,7 @@ random_search= RandomizedSearchCV(
     n_iter=50,
     cv=None,
     scoring='neg_root_mean_squared_error',
-    n_jobs=-1
-)
+    n_jobs=-1)
 
 #fitting the model with random searchCV, take the most time
 model_fit=random_search.fit(X_train, y_train)
@@ -46,3 +50,7 @@ shap_values=explain(X_test)
 #visualizing
 shap.plots.beeswarm(shap_values)
 shap.plots.bar(shap_values)
+
+
+
+
